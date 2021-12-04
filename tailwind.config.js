49225c4809d4cf28900e9d1760f75843cb5db7da
anamdiazs <anamdiazs1212@gmail.com>
2021-12-03 19:14:05 -0600

    Install tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+	  extend: {
+		  backgroundImage: {
+			  'sanFrancisco': "url('../img/sanFrancisco.jpg')",
+			  'yosemite': "url('../img/yosemite.jpg')",
+			  'LA': "url('../img/LA.jpg')",
+			  'seattle': "url('../img/seattle.jpg')",
+			  'new_york': "url('../img/new_york.jpg')",
+			  'norway': "url('../img/norway.jpg')",
+			  'sydney': "url('../img/sydney.jpg')",
+			  'miami': "url('../img/miami.jpg')",
+			  'switzerland': "url('../img/switzerland.jpg')",
+			  'bali': "url('../img/bali.jpg')",
+			  'norway': "url('../img/norway.jpg')",
+			  'chicago': "url('../img/chicago.jpg')",
+			  'europe': "url('../img/europe.jpg')",
+			  'iceland': "url('../img/iceland.jpg')",
+			  
+			},
+			backgroundColor: theme =>({
+				...theme('colors'),
+				'principal': '#CC2D4A',
+				'secondary': '#8FA206',
+				'tertiary': '#61AEC9',
+			}),
+			textColor:{
+				'principal': '#CC2D4A',
+				'secondary': '#8FA206',
+				'tertiary': '#61AEC9',
+			}
+		},
+	},
+	variants: {
+		width: ["responsive", "hover", "focus"],
+		extend: {
+	 },
+  },
+  plugins: [],
+}
